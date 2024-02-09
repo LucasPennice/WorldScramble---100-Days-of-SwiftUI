c4ce22a797e9bb72b9a637d7b635386ffadebe5a
Lucas Pennice <lucaspennice@gmail.com>
2024-02-09 13:15:06 -0300

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Lucas Pennice on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
